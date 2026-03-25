078f31a4f6b9ff9d97ad0ae83663c74a8d56c287
Amal Shaji Michael <183818109+amalshajimichaelk@users.noreply.github.com>
2026-03-25 14:19:03 +0530

    Update pg_rplmt.c

--- pg_rplmt.c
+++ pg_rplmt.c
@@ -1,174 +1,102 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_PAGES 50
-#define MAX_FRAMES 10
+int main() {
+    int n, f;
 
-void printSidewaysTable(int ref[], int table[MAX_FRAMES][MAX_PAGES], int status[], int n, int frames) {
-    printf("\nReference String: ");
-    for (int i = 0; i < n; i++) printf("%d  ", ref[i]);
-  
-printf("\n\n");
-    for (int i = 0; i < frames; i++) {
-        printf("Frame %d:        ", i);
-        for (int j = 0; j < n; j++) {
-            if (table[i][j] == -1) printf("-  ");
-            else printf("%d  ", table[i][j]);
-        }
-        printf("\n");
-    }
+    printf("Enter length of reference string: ");
+    scanf("%d", &n);
 
-    printf("\nStatus:         ");
-    int faults = 0,hit=0;
+    printf("Enter number of frames (1-7): ");
+    scanf("%d", &f);
+
+    int pages[100], frames[10];
+
+    printf("Enter %d page numbers (0-9):\n", n);
     for (int i = 0; i < n; i++) {
-        if (status[i] == 0) {
-            printf("M  ");
-            faults++;
-        } else {
-            printf("H  ");
-            hit ++;
-        }
+        scanf("%d", &pages[i]);
     }
-    printf("\n\nTotal Page Faults = %d\n", faults);
-    printf("Total Page Hits = %d\n", hit);
-}
 
-void fifo(int ref[], int n, int frames) {
-    int frame[MAX_FRAMES], table[MAX_FRAMES][MAX_PAGES], status[MAX_PAGES];
-    int index = 0;
-
-    for (int i = 0; i < frames; i++) frame[i] = -1;
+    printf("\n");
+    int fifoFaults = 0, index = 0;
+    for (int i = 0; i < f; i++) frames[i] = -1;
 
     for (int i = 0; i < n; i++) {
-        int hit = 0;
-        for (int j = 0; j < frames; j++) {
-            if (frame[j] == ref[i]) hit = 1;
-        }
+        int found = 0;
+        for (int j = 0; j < f; j++)
+            if (frames[j] == pages[i]) found = 1;
 
-        if (!hit) {
-            frame[index] = ref[i];
-            index = (index + 1) % frames;
-            status[i] = 0;
-        } else {
-            status[i] = 1;
+        if (!found) {
+            frames[index] = pages[i];
+            index = (index + 1) % f;
+            fifoFaults++;
         }
-
-        for (int j = 0; j < frames; j++) table[j][i] = frame[j];
     }
-    printf("\nFIFO Algorithm");
-    printSidewaysTable(ref, table, status, n, frames);
-}
 
-void lru(int ref[], int n, int frames) {
-    int frame[MAX_FRAMES], table[MAX_FRAMES][MAX_PAGES], status[MAX_PAGES], time[MAX_FRAMES];
-    int counter = 0;
-
-    for (int i = 0; i < frames; i++) {
-        frame[i] = -1;
+    int lruFaults = 0, time[10], counter = 0;
+    for (int i = 0; i < f; i++) {
+        frames[i] = -1;
         time[i] = 0;
     }
 
     for (int i = 0; i < n; i++) {
-        counter++;
-        int hit = 0;
-        for (int j = 0; j < frames; j++) {
-            if (frame[j] == ref[i]) {
-                hit = 1;
-                time[j] = counter;
+        int found = 0;
+        for (int j = 0; j < f; j++) {
+            if (frames[j] == pages[i]) {
+                time[j] = ++counter;
+                found = 1;
             }
         }
 
-        if (!hit) {
-            int pos = 0;
-            for (int j = 1; j < frames; j++) {
-                if (time[j] < time[pos]) pos = j;
-            }
-            frame[pos] = ref[i];
-            time[pos] = counter;
-            status[i] = 0;
-        } else {
-            status[i] = 1;
-        }
+        if (!found) {
+            int min = 0;
+            for (int j = 1; j < f; j++)
+                if (time[j] < time[min]) min = j;
 
-        for (int j = 0; j < frames; j++) table[j][i] = frame[j];
+            frames[min] = pages[i];
+            time[min] = ++counter;
+            lruFaults++;
+        }
     }
-    printf("\nLRU Algorithm");
-    printSidewaysTable(ref, table, status, n, frames);
-}
 
-void optimal(int ref[], int n, int frames) {
-    int frame[MAX_FRAMES], table[MAX_FRAMES][MAX_PAGES], status[MAX_PAGES];
-
-    for (int i = 0; i < frames; i++) frame[i] = -1;
+    int optFaults = 0;
+    for (int i = 0; i < f; i++) frames[i] = -1;
 
     for (int i = 0; i < n; i++) {
-        int hit = 0;
-        for (int j = 0; j < frames; j++) {
-            if (frame[j] == ref[i]) hit = 1;
-        }
+        int found = 0;
+        for (int j = 0; j < f; j++)
+            if (frames[j] == pages[i]) found = 1;
 
-        if (!hit) {
-            int pos = -1, farthest = i;
-            for (int j = 0; j < frames; j++) {
-                if (frame[j] == -1) {
+        if (!found) {
+            int pos = -1, farthest = i + 1;
+
+            for (int j = 0; j < f; j++) {
+                int k;
+                for (k = i + 1; k < n; k++) {
+                    if (frames[j] == pages[k]) {
+                        if (k > farthest) {
+                            farthest = k;
+                            pos = j;
+                        }
+                        break;
+                    }
+                }
+                if (k == n) {
                     pos = j;
                     break;
                 }
             }
 
-            if (pos == -1) {
-                for (int j = 0; j < frames; j++) {
-                    int k;
-                    for (k = i + 1; k < n; k++) {
-                        if (frame[j] == ref[k]) break;
-                    }
-                    if (k > farthest) {
-                        farthest = k;
-                        pos = j;
-                    }
-                }
-            }
             if (pos == -1) pos = 0;
-            frame[pos] = ref[i];
-            status[i] = 0;
-        } else {
-            status[i] = 1;
-        }
 
-        for (int j = 0; j < frames; j++) table[j][i] = frame[j];
-    }
-    printf("\nOptimal Algorithm:");
-    printSidewaysTable(ref, table, status, n, frames);
-}
-
-int main() {
-    int ref[MAX_PAGES], n, frames, choice,hit;
-
-    printf("Enter number of pages: ");
-    scanf("%d", &n);
-    printf("Enter reference string: ");
-    for (int i = 0; i < n; i++) scanf("%d", &ref[i]);
-    printf("Enter number of frames: ");
-    scanf("%d", &frames);
-
-    while (1) {
-        printf("\n1. FIFO\n2. LRU\n3. Optimal\n4. Exit\nChoice: ");
-        scanf("%d", &choice);
-         switch (choice) {
-            case 1: 
-              fifo(ref, n, frames); 
-              break;
-            case 2: 
-              lru(ref, n, frames); 
-              break;
-            case 3: 
-              optimal(ref, n, frames); 
-              break;
-            case 4: 
-              printf("Exiting\n"); 
-              return 0;
-            default: 
-              printf("Invalid choice!\n");
+            frames[pos] = pages[i];
+            optFaults++;
         }
     }
+
+    printf("FIFO Page Faults = %d\n", fifoFaults);
+    printf("LRU Page Faults = %d\n", lruFaults);
+    printf("Optimal Page Faults = %d\n", optFaults);
+
     return 0;
 }
